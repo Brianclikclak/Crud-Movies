31d261c7c2e7b716c42c34b40027f03a19521908
Brianclikclak <brianclikclak@hotmail.com>
2023-08-18 15:18:37 +0200

    testing db conection

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.models.Film;
+import com.example.demo.repository.FilmRespository;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Autowired
+	private FilmRespository respository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Film film1 = new Film("A todo gas", 2000, "Accion");
+		respository.save(film1);
+
+		Film film2 = new Film("A todo gas 2", 2002, "Accion");
+		respository.save(film2);
+
+		Film film3 = new Film("A todo gas 3 ", 2003, "Accion");
+		respository.save(film3);
 	}
 
 }
