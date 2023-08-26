4e433b747686b9ec20d84598fea4e23f024177c0
Brianclikclak <brianclikclak@hotmail.com>
2023-08-26 21:44:04 +0200

    controller for get method

--- demo/src/main/java/com/example/demo/controller/filmController.java
+++ demo/src/main/java/com/example/demo/controller/filmController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.Film;
 import com.example.demo.services.FilmService;
 
 @RestController
@@ -18,9 +20,9 @@ public class FilmController {
     @Autowired
     private FilmService service;
 
-    @GetMapping({ "/films", "/" })
-    public String listFilms(Model model) {
-        model.addAttribute("films", service.listAllFilms());
-        return "films";
+    @GetMapping("/{id}")
+    public Optional<Film> getFilmById(@PathVariable Long id) {
+        return filmService.findById(id);
     }
+
 }
